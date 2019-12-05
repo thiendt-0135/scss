e0eed16bfc58e85241ff4531451107cbce735ef3
thiendt <dinh.trong.thien@sun-asterisk.com>
2019-12-05 10:17:49 +0700

    bug#4-fix-bug-responsive

--- js/master.js
+++ js/master.js
@@ -4,8 +4,6 @@
 $('.js-featured').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
   infinite: true,
   arrows: false,
   dots: true,
@@ -36,11 +34,12 @@ $('.js-featured').slick({
 // carousel list image in instagram
 
 $('.js-post-item').slick({
-  slidesToShow: 1,
   autoplay: true,
   infinite: true,
   autoplaySpeed: 5000,
+  speed: 500,
   fade: true,
+  cssEase: 'linear',
   arrows: false,
 });
 
